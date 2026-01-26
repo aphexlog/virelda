78010396813a7e41dd08784140cd0fe504f78638
Aaron West <aphexlog@github.com>
2026-01-25 18:40:41 -0600

    Fix credits back button visibility by removing expanding spacer

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -3,5 +3,9 @@ extends Control
 func _ready():
 	$MarginContainer/VBoxContainer/BackButton.pressed.connect(_on_back_pressed)
 
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		_on_back_pressed()
+
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://scenes/ui/title_screen.tscn")
